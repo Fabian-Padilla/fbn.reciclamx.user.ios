ed23246edb73dc7dd856b1023905f84ff2d2e755
Guillermo Fabian Padilla Lam <gfabian.lam@gmail.com>
2020-04-30 00:43:46 -0500

    first commit

---
+++ reciclamx.user.ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  reciclamx.user.ios
+//
+//  Created by Guillermo Padilla Lam on 30/04/20.
+//  Copyright © 2020 gp Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
